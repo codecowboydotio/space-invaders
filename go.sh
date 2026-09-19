702a17b1e1e9f8f6be3b1986bac80711ede1d2fd
Scott van Kalken <24376322+codecowboydotio@users.noreply.github.com>
2026-09-19 15:22:34 +1000

    Add comments to go.sh about Python server usage
    Added comments regarding the use of Python HTTP server.

--- go.sh
+++ go.sh
@@ -1,1 +1,3 @@
+# Adding comment on running as a python server raw.
+# This is not the best way to do this and I should probably add a web server at some point.
 python -m http.server 8000 --bind 0.0.0.0
