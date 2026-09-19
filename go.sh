21f09089dd00e47df8042034f5288fc88ff3f041
Scott van Kalken <24376322+codecowboydotio@users.noreply.github.com>
2026-09-19 21:42:27 +1000

    Add comments to go.sh for clarity on server usage
    Added comments to explain the use of the Python web server.

--- go.sh
+++ go.sh
@@ -1,1 +1,4 @@
+# Add a comment to mention that the shell script to start the server does so using the inbuilt python web server.
+# This works, but it is ugly and is a "raw" http server. 
+# This should be fixed in the future.
 python -m http.server 8000 --bind 0.0.0.0
